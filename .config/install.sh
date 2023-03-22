2ba7e99692d68697687c7d8c7c917fff4d7e9f86
Swarsel <leon.schwarzaeugl@gmail.com>
2023-03-22 04:41:49 +0100

    add python language server

--- .config/install.sh
+++ .config/install.sh
@@ -3,7 +3,7 @@ su
 
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf update
-sudo dnf install firefox emacs sway wofi kitten zsh waybar git gh network-manager-applet blueman discord mako flatpak lpf-spotify-client xournalpp -y
+sudo dnf install firefox emacs sway wofi kitten zsh waybar git gh network-manager-applet blueman discord mako flatpak lpf-spotify-client xournalpp wl-clipboard -y
 
 lpf update
 
@@ -12,7 +12,8 @@ flatpak install flathub chat.schildi.desktop --hidden
 sudo timedatectl set-ntp yes
 sudo timedatectl set-local-rtc 0
 
-
+pip install python-lsp-server
+pip install debugpy
 
 cd ~
 echo ".cfg" >> .gitignore
